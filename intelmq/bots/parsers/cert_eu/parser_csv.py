b99106d4e61f9ef8d80ecce12b14ec73b4e542e2
Sebastian Wagner <wagner@cert.at>
2019-04-02 10:35:14 +0200

    BUG: cert-eu parser: update mapping

--- intelmq/bots/parsers/cert_eu/parser_csv.py
+++ intelmq/bots/parsers/cert_eu/parser_csv.py
@@ -32,18 +32,37 @@ class CertEUCSVParserBot(ParserBot):
     })
 
     csv_fieldnames = [
-        "datasource", "source ip", "observation time", "tlp", "description",
-        "type", "count", "source time", "source country", "protocol",
-        "destination port", "source latitude", "source city", "source cc",
-        "source longitude", "first_seen", "num_sensors", "confidence level",
-        "last_seen", "target", "url", "asn", "domain name"]
+        "feed code", "source ip", "source time", "observation time", "tlp", "description",
+        "type", "protocol", "destination port", "first_seen", "last_seen",
+        "count", "source cc", "source country", "source city",
+        "source longitude", "source latitude",
+        "city",  # empty
+        "threat type",
+        "source location",  # just a combination of long and lat
+        "source as name", "source bgp prefix", "source geohash", "confidence level",
+        "source asn", "target", "url", "domain name",
+        "version", "expiration date", "source port", "status", "source",
+        "scanner", "abuse_contact", "ns1", "ns2",
+        "response", "recent", "country",  # empty
+        "as name",  # empty
+        ]
+    unknown_fields = ["threat type", "ns1", "ns2", "response", "recent"]
     ignore_lines_starting = ["#"]
 
     def parse_line(self, line, report):
         event = self.new_event(report)
-        if "datasource" in line:
-            event["extra.datasource"] = line["datasource"]
+        if line["version"] != "1.5":
+            raise ValueError("Unknown version %r. Please report this with an example."
+                             "" % line["version"])
+        for unknown in self.unknown_fields:
+            if unknown in line:
+                raise ValueError("Unable to parse field %r. Please report this with an example"
+                                 "" % unknown)
+
+        if "feed code" in line:
+            event["extra.datasource"] = line["feed code"]
         event.add("source.ip", line["source ip"])
+        event.add("source.network", line["source bgp prefix"])
         event.add("extra.cert_eu_time_observation",
                   DateTime.sanitize(line["observation time"]))
         event.add("tlp", line["tlp"])
@@ -59,18 +78,27 @@ class CertEUCSVParserBot(ParserBot):
         event.add("source.geolocation.city", line["source city"])
         event.add("source.geolocation.geoip_cc", line["source cc"])
         event.add("source.geolocation.longitude", line["source longitude"])
+        event.add("extra.source.geolocation.geohash", line["source geohash"])
         if "first_seen" in line:
             event["extra.first_seen"] = line["first_seen"]
         if "num_sensors" in line:
             event["extra.num_sensors"] = line["num_sensors"]
-        event.add('feed.accuracy',
-                  event.get('feed.accuracy', 100) * int(line["confidence level"]) / 100,
-                  overwrite=True)
+        if line["confidence level"] != '':
+            event.add('feed.accuracy',
+                      event.get('feed.accuracy', 100) * int(line["confidence level"]) / 100,
+                      overwrite=True)
         if "last_seen" in line:
             event["extra.last_seen"] = line["last_seen"]
+        if "expiration date" in line:
+            event["extra.expiration_date"] = line["expiration date"]
+        if "status" in line:
+            event["status"] = line["status"]
         event.add("event_description.target", line["target"])
         event.add("source.url", line["url"])
+        event.add("source.port", line["port"])
+        event.add("source.abuse_contact", line["abuse contact"])
         event.add("source.asn", line["source asn"])
+        event.add("source.as_name", line["source as name"])
         event.add("source.fqdn", line["domain name"])
 
         event.add("raw", self.recover_line(line))
