a56870cce9446921557336ac29377dd4d86ca23f
Sebastian Wagner <wagner@cert.at>
2019-08-13 10:25:04 +0200

    experiment: use retry of requests for do-portal

--- intelmq/bots/experts/do_portal/expert.py
+++ intelmq/bots/experts/do_portal/expert.py
@@ -21,6 +21,20 @@ class DoPortalExpertBot(Bot):
             "API-Authorization": self.parameters.portal_api_key
         })
         self.mode = self.parameters.mode
+        self.session = requests.Session()
+        self.session.headers = self.http_header
+        self.session.auth = self.auth
+        self.session.proxies = self.proxy
+        self.session.cert = self.ssl_client_cert
+        self.session.timeout = self.http_timeout_sec
+        self.session.verify = self.http_verify_cert
+        retries = requests.urllib3.Retry.from_int(self.http_timeout_max_tries)
+        retries.status_forcelist = [502]  # Frontend reverse-proxies the (backend) API
+        adapter = requests.adapters.HTTPAdapter(max_retries=retries)
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
+        self.session.proxies = self.proxy
+        self.session.auth = self.auth
 
     def process(self):
         event = self.receive_message()
@@ -29,24 +43,26 @@ class DoPortalExpertBot(Bot):
             self.acknowledge_message()
             return
 
-        timeoutretries = 0
+#        timeoutretries = 0
         req = None
 
-        while timeoutretries < self.http_timeout_max_tries and req is None:
-            try:
-                req = requests.get(self.url % event['source.ip'],
-                                   headers=self.http_header,
-                                   auth=self.auth,
-                                   proxies=self.proxy,
-                                   verify=self.http_verify_cert,
-                                   cert=self.ssl_client_cert,
-                                   timeout=self.http_timeout_sec)
-            except requests.exceptions.Timeout:
-                timeoutretries += 1
+        req = self.session.get(self.url % event['source.ip'])
 
-        if req is None and timeoutretries >= self.http_timeout_max_tries:
-            raise ValueError("Request timed out %i times in a row."
-                             "" % timeoutretries)
+#        while timeoutretries < self.http_timeout_max_tries and req is None:
+#            try:
+#                req = requests.get(self.url % event['source.ip'],
+#                                   headers=self.http_header,
+#                                   auth=self.auth,
+#                                   proxies=self.proxy,
+#                                   verify=self.http_verify_cert,
+#                                   cert=self.ssl_client_cert,
+#                                   timeout=self.http_timeout_sec)
+#            except requests.exceptions.Timeout:
+#                timeoutretries += 1
+
+#        if req is None and timeoutretries >= self.http_timeout_max_tries:
+#            raise ValueError("Request timed out %i times in a row."
+#                             "" % timeoutretries)
 
         if req.status_code == 404 and req.json()['message'].startswith("('no such cidr'"):
             result = []
